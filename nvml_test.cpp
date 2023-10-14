c567865b8c7f8a958e8f42b36797c844c255495a
Sean <seany071305@gmail.com>
2023-10-14 15:55:34 -0400

    rewrote nvml_test.cpp using error macro

--- nvml_test.cpp
+++ nvml_test.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
 #include <nvml.h>
 
-int main() {
-    nvmlReturn_t result;
-    result = nvmlInit();
-    if (result != NVML_SUCCESS) {
-        std::cerr << "NVML initialization failed: " << nvmlErrorString(result) << std::endl;
-        return 1;
-    }
-    unsigned int deviceCount; // Change the data type to unsigned int
-    result = nvmlDeviceGetCount(&deviceCount);
-    if (result != NVML_SUCCESS) {
-        std::cerr << "Failed to get the number of GPU devices: " << nvmlErrorString(result) << std::endl;
-        nvmlShutdown();
-        return 1;
-    }
+// define a macro that returns the errorstring if the nvml call fails
+#define NVML_TRY(call)                                                                 \
+    do                                                                                 \
+    {                                                                                  \
+        nvmlReturn_t result = call;                                                    \
+        if (result != NVML_SUCCESS)                                                    \
+        {                                                                              \
+            std::cerr << "NVML call failed: " << nvmlErrorString(result) << std::endl; \
+            nvmlShutdown();                                                            \
+            return 1;                                                                  \
+        }                                                                              \
+    } while (0)
 
-    if (deviceCount == 0) {
+int main()
+{
+    NVML_TRY(nvmlInit());
+
+    unsigned int deviceCount; // Change the data type to unsigned int
+
+    NVML_TRY(nvmlDeviceGetCount(&deviceCount));
+    if (deviceCount == 0)
+    {
         std::cerr << "No GPU devices found." << std::endl;
         nvmlShutdown();
         return 1;
     }
 
-    for (unsigned int i = 0; i < deviceCount; i++) { // Change the data type to unsigned int
+    // Loop through the number of devices and print the name for each
+    for (unsigned int i = 0; i < deviceCount; i++)
+    {
         nvmlDevice_t device;
-        result = nvmlDeviceGetHandleByIndex(i, &device);
-        if (result != NVML_SUCCESS) {
-            std::cerr << "Failed to get the handle for GPU device " << i << ": " << nvmlErrorString(result) << std::endl;
-            continue;
-        }
+        NVML_TRY(nvmlDeviceGetHandleByIndex(i, &device));
 
-        char name[NVML_DEVICE_NAME_BUFFER_SIZE];
-        result = nvmlDeviceGetName(device, name, NVML_DEVICE_NAME_BUFFER_SIZE);
-        if (result != NVML_SUCCESS) {
-            std::cerr << "Failed to get the name of GPU device " << i << ": " << nvmlErrorString(result) << std::endl;
-        } else {
-            std::cout << "GPU " << i << " Name: " << name << std::endl;
-        }
+        char name[NVML_DEVICE_NAME_V2_BUFFER_SIZE];
+        NVML_TRY(nvmlDeviceGetName(device, name, NVML_DEVICE_NAME_V2_BUFFER_SIZE));
+        std::cout << "GPU number " << i << " name is: " << name << std::endl;
     }
 
     nvmlShutdown();
