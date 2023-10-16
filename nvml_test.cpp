f63b0a7668f2f750876897d17343c72d0cdbdee3
Sean <seany071305@gmail.com>
2023-10-16 14:17:36 -0400

    added GPU clock, memory clock, video decoder clock, fan speed, and temperature monitoring

--- nvml_test.cpp
+++ nvml_test.cpp
@@ -31,12 +31,35 @@ int main()
     // Loop through the number of devices and print the name for each
     for (unsigned int i = 0; i < deviceCount; i++)
     {
-        nvmlDevice_t device;
-        NVML_TRY(nvmlDeviceGetHandleByIndex(i, &device));
+        for (unsigned int i = 0; i < deviceCount; i++)
+        {
+            nvmlDevice_t device;
+            NVML_TRY(nvmlDeviceGetHandleByIndex(i, &device));
 
-        char name[NVML_DEVICE_NAME_V2_BUFFER_SIZE];
-        NVML_TRY(nvmlDeviceGetName(device, name, NVML_DEVICE_NAME_V2_BUFFER_SIZE));
-        std::cout << "GPU number " << i << " name is: " << name << std::endl;
+            char name[NVML_DEVICE_NAME_V2_BUFFER_SIZE];
+            NVML_TRY(nvmlDeviceGetName(device, name, NVML_DEVICE_NAME_V2_BUFFER_SIZE));
+            std::cout << "GPU number " << i << " name is: " << name << std::endl;
+
+            unsigned int graphics_clock;
+            NVML_TRY(nvmlDeviceGetClockInfo(device, NVML_CLOCK_GRAPHICS, &graphics_clock));
+            std::cout << "GPU number " << i << " graphics clock is: " << graphics_clock << " MHz" << std::endl;
+
+            unsigned int memory_clock;
+            NVML_TRY(nvmlDeviceGetClockInfo(device, NVML_CLOCK_MEM, &memory_clock));
+            std::cout << "GPU number " << i << " memory clock is: " << memory_clock << " MHz" << std::endl;
+
+            unsigned int decoder_clock;
+            NVML_TRY(nvmlDeviceGetClockInfo(device, NVML_CLOCK_VIDEO, &decoder_clock));
+            std::cout << "GPU number " << i << " decoder clock is: " << decoder_clock << " MHz" << std::endl;
+
+            /*unsigned int fan_speed;
+            NVML_TRY(nvmlDeviceGetFanSpeed(device, &fan_speed));
+            std::cout << "GPU number " << i << " fan speed is: " << fan_speed << " %" << std::endl;
+            */
+
+            unsigned int temperature;
+            NVML_TRY(nvmlDeviceGetTemperature(device, NVML_TEMPERATURE_GPU, &temperature));
+            std::cout << "GPU number " << i << " temperature is: " << temperature << " C" << std::endl;
     }
 
     nvmlShutdown();
