42d3c46290a7aa495ad7afaa82f94cdb107a6129
Sean <seany071305@gmail.com>
2023-10-16 17:09:15 -0400

    added console out for performance stats

--- nvml_test.cpp
+++ nvml_test.cpp
@@ -122,6 +122,26 @@ private:
 int
 main()
 {
+    unsigned int deviceIndex{0};
+
+    nvmlClass nvmlObj(deviceIndex);
+
+    while(true){
+        std::cout << "The GPU name is: " << nvmlObj.getName() << std::endl;
+        nvmlObj.updatePerfStats();
+        std::cout << "The GPU graphics clock is: " << nvmlObj.getGraphicsClock() << " MHz" << std::endl;
+        std::cout << "The GPU memory clock is: " << nvmlObj.getMemoryClock() << " MHz" << std::endl;
+        std::cout << "The GPU decoder clock is: " << nvmlObj.getDecoderClock() << " MHz" << std::endl;
+        //std::cout << "The GPU fan speed is: " << nvmlObj.getFanSpeed() << " %" << std::endl;
+        std::cout << "The GPU temperature is: " << nvmlObj.getTemperature() << " C" << std::endl;
+        std::cout << "The GPU memory total is: " << nvmlObj.getMemoryTotal() << " MB" << std::endl;
+        std::cout << "The GPU memory used is: " << nvmlObj.getMemoryUsed() << " MB" << std::endl;
+        std::cout << "The GPU memory free is: " << nvmlObj.getMemoryFree() << " MB" << std::endl;
+        std::cout << std::endl;
+        Sleep(1000);
+    }
+
+    /*
     NVML_TRY(nvmlInit());
 
     unsigned int deviceCount; // Change the data type to unsigned int
