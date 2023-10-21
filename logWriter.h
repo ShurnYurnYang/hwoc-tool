a25651146db6bbb07fac76cf0c8f82139dfdd8b6
ShurnYurnYang <seany071305@gmail.com>
2023-10-21 19:34:32 -0400

    added methods and public/private members

--- logWriter.h
+++ logWriter.h
@@ -5,8 +5,27 @@
 #include <fstream>
 #include <ctime>
 
+
 class logWriter
 {
     public:
+        logWriter(const std::string filename) : filename_(filename){
+            logFile_.open(filename_, std::ios::out | std::ios::app);
+        }
+
+        ~logWriter(){
+            logFile_.close();
+        }
+
+        void writeEntry(const std::string entryMessage){
+            std::time_t now = std::time(0);
+            char timestamp[100];
+            std::strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+            logFile_ << timestamp << " " << entryMessage << std::endl;
+        }
+    private:
+        const std::string filename_;
+        std::ofstream logFile_;
+}
     
     
