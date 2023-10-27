300763c4b9c3e90a685e74a1a05d8a4cecc6f195
Sean <seany071305@gmail.com>
2023-10-27 16:55:22 -0400

    removed depracated powershell files

--- test.ps1
+++
@@ -1,8 +0,0 @@
-$Processor = Get-WmiObject Win32_Processor
-$MaxClockSpeed = $Processor.MaxClockSpeed
-
-$ProcessorPerformance = Get-Counter -Counter "\Processor Information(_Total)\% Processor Performance" | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue
-
-$CurrentClockSpeed = $MaxClockSpeed * ($ProcessorPerformance / 100)
-
-Write-Host $CurrentClockSpeed
